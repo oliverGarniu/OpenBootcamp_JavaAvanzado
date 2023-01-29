655b711225bdf5e7baea2c980d587afbc0ee3791
oliverGarniu <oliver.garniu@gmail.com>
2023-01-29 19:38:45 +0100

    limpiar BootcamperService ejercicio 3

--- OB_JavaAvanzado/sesiones_19_20_21/ejercicio3/src/main/java/com/ob/ejercicio3/controller/BootcamperController.java
+++ OB_JavaAvanzado/sesiones_19_20_21/ejercicio3/src/main/java/com/ob/ejercicio3/controller/BootcamperController.java
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Component;
 import java.net.URI;
 import java.util.List;
 
-/**
- * Controller de Bootcamper donde incluimos todas las peticiones.
- * Además se crean 3 bootcampers para no tener una lista vacía
- * cuando iniciemos la aplicación.
+/*
+ * Controller de Bootcamper donde incluimos las peticiones.
+ * Además se crean y añaden 3 bootcampers para no tener una lista vacía
+ * cuando iniciamos la aplicación.
  */
 @Component
 @Path("/api")
@@ -36,8 +36,7 @@ public class BootcamperController {
     }
 
     /**
-     * Devuelve la info de un bootcamper introduciendo
-     * su nombre como parámetro.
+     * Devuelve un bootcamper introduciendo su nombre como parámetro.
      *
      * @param name nombre del bootcamper
      * @return bootcamper Devuelve un bootcamper si existe
@@ -65,8 +64,8 @@ public class BootcamperController {
     @Produces("application/json")
     @Consumes("application/json")
     public Response addBootcamper(Bootcamper bootcamper) {
-        Bootcamper existBootcamper = bootcamperService.addBootcamper(bootcamper);
-        return existBootcamper == null ? Response.created(URI.create("/bootcampers/" + bootcamper.getName()))
+        Bootcamper bc = bootcamperService.addBootcamper(bootcamper);
+        return bc == null ? Response.created(URI.create("/bootcampers/" + bootcamper.getName()))
                 .build() : Response.noContent().build();
     }
 
@@ -76,13 +75,13 @@ public class BootcamperController {
      *
      * @param name nombre del bootcamper a eliminar
      * @return devuelve un String indicando si ha encontrado y eliminado
-     * al bootcamper, o bien si no existe ningún bootcamper con ese nombre
+     * al bootcamper, o bien que no existe ningún bootcamper con ese nombre
      * en la lista.
      */
     @DELETE
     @Path("/bootcampers/{name}")
-    public String deleteOneBootcamper(@PathParam("name") String name) {
-        return bootcamperService.deleteBootcamper(name);
+    public void deleteOneBootcamper(@PathParam("name") String name) {
+        bootcamperService.deleteBootcamper(name);
     }
 
     @DELETE
--- OB_JavaAvanzado/sesiones_19_20_21/ejercicio3/src/main/java/com/ob/ejercicio3/service/BootcamperService.java
+++ OB_JavaAvanzado/sesiones_19_20_21/ejercicio3/src/main/java/com/ob/ejercicio3/service/BootcamperService.java
@@ -6,14 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-//import java.util.Iterator;
 import java.util.List;
 import java.util.OptionalDouble;
 
-/**
- * En BootcamperService aplicamos toda la lógica necesaria para poder
- * ejecutar las peticiones llevadas a cabo en el controller.
- */
 @Service
 public class BootcamperService {
 
@@ -28,7 +23,15 @@ public class BootcamperService {
     }
 
     public Bootcamper getBootcamper(String name) {
-        return findBootcamper(name);
+        boolean found = bootcamperExists(name);
+
+        if (found) {
+            logger.info("Bootcamper " + name + " found successfully");
+            return getActualBootcamper(name);
+        }
+
+        logger.info("Bootcamper " + name + " not found");
+        return null;
     }
 
     public String getAverageAge() {
@@ -40,67 +43,41 @@ public class BootcamperService {
     }
 
     public Bootcamper addBootcamper(Bootcamper bootcamper) {
-        Bootcamper existBootcamper = findBootcamper(bootcamper.getName());
+        boolean found = bootcamperExists(bootcamper.getName());
 
-        if (existBootcamper == null) {
-            bootcamperList.add(bootcamper);
-            logger.info("Bootcamper " + bootcamper.getName().toUpperCase() + " added successfully");
-            return null;
+        if (found) {
+            logger.info("Bootcamper " + bootcamper.getName().toUpperCase() + " already exists");
+            return bootcamper;
         }
 
-        logger.info("Bootcamper " + bootcamper.getName().toUpperCase() + " already exists");
-        return bootcamper;
+        bootcamperList.add(bootcamper);
+        logger.info("Bootcamper " + bootcamper.getName().toUpperCase() + " added successfully");
+        return null;
     }
 
-    /**
-     * Se añaden 3 posibles maneras de eliminar un bootcamper
-     * de la lista a partir del nombre: Usando foreach, iterator o stream.
-     * <p>
-     * Dejamos dos de las opciones comentadas para poder ejecutar el programa.
-     */
-    public String deleteBootcamper(String name) {
-        Bootcamper existBootcamper = findBootcamper(name);
+    public void deleteBootcamper(String name) {
+        boolean found = bootcamperExists(name);
 
-        if (existBootcamper != null) {
-            bootcamperList.remove(existBootcamper);
-            return "Bootcamper " + name.toUpperCase() + " deleted successfully";
+        if (found) {
+            Bootcamper bootcamper = getActualBootcamper(name);
+            bootcamperList.remove(bootcamper);
+            logger.info("Bootcamper " + name.toUpperCase() + " removed successfully");
+        } else {
+            logger.info("Bootcamper " + name.toUpperCase() + " not found");
         }
-        return "Bootcamper " + name.toUpperCase() + " not found";
     }
-
-//    // mismo método que el anterior pero utilizando iterator
-//    public String deleteBootcamper(String name) {
-//        // reinicio el iterador, volviendo a la posición 0.
-//        Iterator<Bootcamper> iter = bootcamperList.iterator();
-//
-//        while (iter.hasNext()) {
-//            Bootcamper bootcamper = iter.next();
-//            if (bootcamper.getName().equalsIgnoreCase(name)) {
-//                bootcamperList.remove(bootcamper);
-//                return "Bootcamper " + name.toUpperCase() + " deleted successfully";
-//            }
-//        }
-//        return "Bootcamper " + name.toUpperCase() + " not found";
-//    }
-
-//    // mismo método que el anterior pero utilizando stream
-//    public String deleteBootcamper(String name) {
-//        boolean existBootcamper = bootcamperList.stream()
-//                .anyMatch(bootcamper -> bootcamper.getName().equalsIgnoreCase(name));
-//
-//        if (existBootcamper) {
-//            bootcamperList.remove(getBootcamper(name));
-//            return "Bootcamper " + name.toUpperCase() + " deleted successfully";
-//        }
-//        return "Bootcamper " + name.toUpperCase() + " not found";
-//    }
 
     public void deleteAllBootcampers() {
         bootcamperList.clear();
         logger.info("Bootcamper list is empty");
     }
 
-    public Bootcamper findBootcamper(String name) {
+    public boolean bootcamperExists(String name) {
+        return bootcamperList.stream()
+                .anyMatch(bootcamper -> bootcamper.getName().equalsIgnoreCase(name));
+    }
+
+    public Bootcamper getActualBootcamper(String name) {
         return bootcamperList.stream()
                 .filter(bootcamper -> bootcamper.getName().equalsIgnoreCase(name))
                 .findFirst().orElse(null);
